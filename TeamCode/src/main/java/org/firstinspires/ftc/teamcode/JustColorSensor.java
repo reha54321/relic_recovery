9896b5c1e0145b4806e94b6e8b2ab546a399a048
Stewart Hughes <stewvhughes@gmail.com>
2017-09-20 17:33:48 -0400

    Input Sensor Methods
    including added wait method for future efficiency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustColorSensor.java
@@ -50,5 +50,55 @@ public class ColorSensorTest extends OpMode {
         telemetry.update();
 
     }
+    
+    
+       //SENSOR CODE
+   public void sense(){
+       // hsvValues is an array that will hold the hue, saturation, and value information.
+       float hsvValues[] = {0F,0F,0F};
+
+       // values is a reference to the hsvValues array.
+       final float values[] = hsvValues;
+
+       // get a reference to the RelativeLayout so we can change the background
+       // color of the Robot Controller app to match the hue detected by the RGB sensor.
+       final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
+    
+
+       // bLedOn represents the state of the LED.
+       boolean bLedOn = true;
+
+       // turn the LED on in the beginning, just so user will know that the sensor is active.
+       colorSensor.enableLed(bLedOn);
+
+       colorSensor = hardwareMap.colorSensor.get("sensor_color");
+
+       // convert the RGB values to HSV values.
+       Color.RGBToHSV(colorSensor.red(), colorSensor.green(), colorSensor.blue(), hsvValues);
+
+       // send the info back to driver station using telemetry function.
+       telemetry.addData("LED", bLedOn ? "On" : "Off");
+       telemetry.addData("Clear", colorSensor.alpha());
+       telemetry.addData("Red ", colorSensor.red());
+       telemetry.addData("Green", colorSensor.green());
+       telemetry.addData("Blue ", colorSensor.blue());
+       telemetry.addData("Hue", hsvValues[0]);
+      // telemetry.addData("Is Red: ", (colorSensor.red()>200) && (colorSensor.blue()<100));
+      // telemetry.addData("Is Blue: ", (colorSensor.blue()>200) && (colorSensor.red()<100));
+    
+       // change the background color to match the color detected by the RGB sensor.
+       // pass a reference to the hue, saturation, and value array as an argument
+       // to the HSVToColor method.
+       //pass HSV values to Driver Station phones, thereby changing phone backgrounds to RGB color values
+   }
+
+
+   public void wait(int time){
+       try{
+           Thread.sleep(time * 1000);
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+   }
 }
 

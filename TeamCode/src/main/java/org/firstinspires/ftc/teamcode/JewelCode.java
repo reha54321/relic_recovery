92440f0d87977266a45a00a582277d6f591dd138
Eleanor Mather <eleanor_mather@horacemann.org>
2017-09-27 10:28:59 -0400

    fixed runtime() issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelCode.java
@@ -6,6 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
+import java.lang.Runtime;
+import 	java.util.Timer;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -14,11 +20,25 @@ import android.graphics.Color;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import android.app.Activity;
 import android.view.View;
+//package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import java.lang.Object;
+
+
 
 
 @Autonomous(name="ColorSensorTest", group="Autonomous")
 
-public class ColorSensorTest extends OpMode {
+public class JewelCode extends OpMode {
     final boolean DEBUG = true;
     DcMotor leftBack;
     DcMotor leftFront;
@@ -27,7 +47,7 @@ public class ColorSensorTest extends OpMode {
     ColorSensor testSense;
     Servo continuousServo;
     String whichColor;
-    
+    private ElapsedTime runtime = new ElapsedTime();
     @Override
     public void init(){
         continuousServo.setPosition(0.0);
@@ -47,11 +67,44 @@ public class ColorSensorTest extends OpMode {
         // color of the Robot Controller app to match the hue detected by the RGB sensor.
         final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
 
-        colorSensor.enableLed(false);
+        testSense.enableLed(false);
 
-        waitForStart();
+        //waitForStart();
 
-        Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+        Color.RGBToHSV(testSense.red() * 8, testSense.green() * 8, testSense.blue() * 8, hsvValues);
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+
+
+        // values is a reference to the hsvValues array.
+
+
+
+        // bLedOn represents the state of the LED.
+        boolean bLedOn = true;
+
+        // turn the LED on in the beginning, just so user will know that the sensor is active.
+        testSense.enableLed(bLedOn);
+
+        testSense = hardwareMap.colorSensor.get("sensor_color");
+
+        // convert the RGB values to HSV values.
+        Color.RGBToHSV(testSense.red(), testSense.green(), testSense.blue(), hsvValues);
+
+        // send the info back to driver station using telemetry function.
+        telemetry.addData("LED", bLedOn ? "On" : "Off");
+        telemetry.addData("Clear", testSense.alpha());
+        telemetry.addData("Red ", testSense.red());
+        telemetry.addData("Green", testSense.green());
+        telemetry.addData("Blue ", testSense.blue());
+        telemetry.addData("Hue", hsvValues[0]);
+        // telemetry.addData("Is Red: ", (colorSensor.red()>200) && (colorSensor.blue()<100));
+        // telemetry.addData("Is Blue: ", (colorSensor.blue()>200) && (colorSensor.red()<100));
+
+        // change the background color to match the color detected by the RGB sensor.
+        // pass a reference to the hue, saturation, and value array as an argument
+        // to the HSVToColor method.
+        //pass HSV values to Driver Station phones, thereby changing phone backgrounds to RGB color values
 
         telemetry.update();
 
@@ -59,12 +112,12 @@ public class ColorSensorTest extends OpMode {
 
         //assuming red team
         
-        if(DEBUG){
+       /* if(DEBUG){
             telemetry.addData("Text:", "isColor Data(4): " + isColor(colorSensor));
             //telemetry.addData("Text:", "RGB Colors: " + colorSensor.red() + ", " + colorSensor.green() + ", " + colorSensor.blue());
             telemetry.addData("Colors (R, B, G)", hsvValues[0]);
             
-        }
+        }*/
     }
 
 
@@ -74,7 +127,7 @@ public class ColorSensorTest extends OpMode {
         
 
     public void checkColor(){
-        if(colorSensor.red>colorSensor.blue){
+        if(testSense.red()>testSense.blue()){
             whichColor="red";
 
         }
@@ -93,19 +146,21 @@ public class ColorSensorTest extends OpMode {
         }
 
         else { //extra precaution
-            while (newTime > 0 && runtime.milliseconds() <= (runtime.milliseconds() + (newTime * 100))) { //1s = 1000ms
+            double currentTime = runtime.milliseconds();
+            while (newTime > 0 && runtime.milliseconds() <= (currentTime + (newTime * 100))) { //1s = 1000ms
                 leftFront.setPower(pos1 / 10);
                 leftBack.setPower(pos2 / 10);
                 rightFront.setPower(pos3 / 10);
                 rightBack.setPower(pos4 / 10);
                 wait(5);
 
-                runtime.reset();
+                //runtime.reset();
             }
         }
-    }
+        }
 
-}
+
+
         @Override
         public void start() {
             checkColor();
@@ -116,7 +171,7 @@ public class ColorSensorTest extends OpMode {
 
             testSense = hardwareMap.colorSensor.get("colorSensor");
 
-            continuousServo = hardwareMap.Servo.get("servo");
+            continuousServo = hardwareMap.servo.get("servo");
 
             leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -140,5 +195,12 @@ public class ColorSensorTest extends OpMode {
 
 
         }
+    public void wait(int time){
+        try{
+            Thread.sleep(time * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
